e8624076267146cf8ed3d0c57c982d05230e95ae
Willian Prado <willian_prado@hotmail.com>
2022-05-16 19:26:49 -0300

    feat(api): add user router to express app

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,8 @@
 import App from "./App";
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import userRouter from '../routes/UserRouter';
+
 dotenv.config();
 
 const PORT = process.env.PORT;
@@ -9,4 +11,5 @@ const MONGODB_URL = process.env.MONGODB_URL;
 const myApp = new App();
 mongoose.connect(`${MONGODB_URL}`);
 
+myApp.addRouter(userRouter);
 myApp.startServer(PORT);
