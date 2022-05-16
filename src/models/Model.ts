08a5602d0bd03f76b7489e8e309919a9411844f6
Willian Prado <willian_prado@hotmail.com>
2022-05-16 15:58:23 -0300

    feat(models): add `Model` class

---
+++ src/models/Model.ts
@@ -0,0 +1,25 @@
+import { Model as M } from "mongoose"
+
+export default abstract class Model<T> {
+  protected schema: M<T>;
+
+  constructor(schema: M<T>) {
+    this.schema = schema;
+  }
+
+  public async create(data: T): Promise<T> {
+    return this.schema.create(data);
+  }
+
+  public async getAll(): Promise<T[]> {
+    return this.schema.find();
+  }
+
+  public async getById(id: string): Promise<T | null> {
+    return this.schema.findById(id);
+  }
+
+  public async getByField(field: string, value: string ): Promise<T | null> {
+    return this.schema.findOne().where(field, value);
+  }
+}
