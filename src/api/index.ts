d84e1934b7f6d3e5ea1c920029a2f98f36d071e1
Willian Prado <willian_prado@hotmail.com>
2022-05-16 13:48:53 -0300

    feat(api): add mongoose connection

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,12 @@
 import App from "./App";
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 dotenv.config();
 
-const myApp = new App();
+const PORT = process.env.PORT;
+const MONGODB_URL = process.env.MONGODB_URL;
 
-myApp.startServer(process.env.PORT);
+const myApp = new App();
+mongoose.connect(`${MONGODB_URL}`);
+
+myApp.startServer(PORT);
