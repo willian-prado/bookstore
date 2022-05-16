7cf4271362ab8b39e6b1e892981a418acfc49e60
Willian Prado <willian_prado@hotmail.com>
2022-05-16 12:47:28 -0300

    feat(api): add `App` class

---
+++ src/api/App.ts
@@ -0,0 +1,22 @@
+import express, { Request as Req, Response as Res } from 'express';
+
+export default class App {
+  private _app: express.Application;
+  get app(): express.Application { return this._app }
+
+  constructor() {
+    this._app = express();
+    this._app.use(express.json());
+  }
+
+  public startServer(port: string | number = 3001): void {
+    this._app.get('/', (_req: Req, res: Res) => res.status(200).json('Hello World!'));
+    this._app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  }
+
+  public addRouter(router: express.Router): void {
+    this._app.use(router);
+  }
+}
