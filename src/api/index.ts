c432dc61d0ec9d263dfdd529736a14ec2841d45b
Willian Prado <willian_prado@hotmail.com>
2022-05-17 15:49:56 -0300

    feat(api): add error handler middleware to express app

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,15 +1,20 @@
 import App from "./App";
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import userRouter from '../routes/UserRouter';
+import UserRouter from '../routes/UserRouter';
+import LoginRouter from "../routes/LoginRouter";
+import ErrorHandler from '../middlewares/errors/ErrorHandler';
 
 dotenv.config();
 
-const PORT = process.env.PORT;
-const MONGODB_URL = process.env.MONGODB_URL;
-
 const myApp = new App();
-mongoose.connect(`${MONGODB_URL}`);
+mongoose.connect(`${process.env.MONGODB_URL}`);
 
-myApp.addRouter(userRouter);
-myApp.startServer(PORT);
+const userRouter = new UserRouter();
+const loginRouter = new LoginRouter();
+
+myApp.addRouter(userRouter.router);
+myApp.addRouter(loginRouter.router);
+myApp.addErrorHandler(ErrorHandler.handle);
+
+myApp.startServer(process.env.PORT);
