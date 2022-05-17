995ff5d4bed61048a943e865bf25c6f4a511ee3c
Willian Prado <willian_prado@hotmail.com>
2022-05-17 15:19:29 -0300

    feat(validations): add joi schema for user

---
+++ src/utils/validations/UserSchema.ts
@@ -0,0 +1,20 @@
+import Joi from "joi";
+
+export default Joi.object().keys({
+  name: Joi.string().min(3).required().messages({
+    'string.base': 'Name must be a string',
+    'string.min': 'Name must be longer than 2 characters',
+    'any.required': 'Name is required',
+  }),
+  password: Joi.string().min(8).required().messages({
+    'string.base': 'Password must be a string',
+    'string.min': 'Password must be longer than 7 characters',
+    'any.required': 'Password is required',
+  }),
+  email: Joi.string().email().required().messages({
+    'string.base': 'Email must be a string',
+    'string.email': 'Email must be a valid email',
+    'any.required': 'Email is required',
+  }),
+});
+
