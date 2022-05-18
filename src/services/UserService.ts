e1d2bcb0b960283dd37cd1a7e863186c42c954db
Willian Prado <willian_prado@hotmail.com>
2022-05-18 18:52:40 -0300

    feat(services): add `update` and `delete` methods to `UserService`

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,9 +1,9 @@
 import bcrypt from 'bcryptjs';
-import { IUser } from "../utils/interfaces/IUser";
-import Service from "./Service";
-import UserModel from "../models/UserModel";
-import AuthUser from "../utils/auth/AuthUser";
-import { EMAIL_REGISTERED } from "../utils/errors";
+import { IUser } from '../utils/interfaces/IUser';
+import Service from './Service';
+import UserModel from '../models/UserModel';
+import AuthUser from '../utils/auth/AuthUser';
+import { EMAIL_REGISTERED } from '../utils/errors';
 import Model from '../models/Model';
 
 export default class UserService extends Service<IUser> {
@@ -14,7 +14,7 @@ export default class UserService extends Service<IUser> {
   public create = async (user: IUser): Promise<string> => {
     const { name, email, password } = user;
 
-    const findUser = await this._model.getByField('email', email); 
+    const findUser = await this._model.getByField('email', email);
     if (findUser) throw EMAIL_REGISTERED;
 
     const salt = bcrypt.genSaltSync(10);
@@ -23,13 +23,16 @@ export default class UserService extends Service<IUser> {
     const newUser = await this._model.create({ name, email, password: passwordHash });
     const token = AuthUser.createToken({ _id: newUser._id, email, name });
     return token;
-  }
+  };
 
-  public getAll = async (): Promise<IUser[]> => {
-    return this._model.getAll();
-  }
+  public getAll = async (): Promise<IUser[]> => this._model.getAll();
 
-  public getById = async (id: string): Promise<IUser | null> => {
-    return this._model.getById(id);
+  public getById = async (id: string): Promise<IUser | null> => this._model.getById(id);
+
+  public update = async (id: string, user: IUser): Promise<IUser | null> => (
+    this._model.update(id, user));
+
+  public delete(id: string): Promise<IUser | null> {
+    return this._model.delete(id);
   }
 }
