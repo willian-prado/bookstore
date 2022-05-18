212d52dca0c7470377f769d8e13ee51c8fbc7ccf
Willian Prado <willian_prado@hotmail.com>
2022-05-18 19:18:44 -0300

    feat(auth): add `validateToken` method

--- src/utils/auth/AuthUser.ts
+++ src/utils/auth/AuthUser.ts
@@ -1,5 +1,7 @@
-import { IDecodedToken } from "../interfaces/IUser";
+import { Request as Req, Response as Res, NextFunction as Next } from 'express';
 import jwt, { SignOptions } from 'jsonwebtoken';
+import { IDecodedToken } from '../interfaces/IUser';
+import { TOKEN_INVALID, TOKEN_NOTFOUND } from '../errors';
 
 export default class AuthUser {
   static jwtConfig: SignOptions = { expiresIn: '1h' };
@@ -8,5 +10,22 @@ export default class AuthUser {
     const payload = { user };
     const token = jwt.sign(payload, process.env.JWT_SECRET as string, AuthUser.jwtConfig);
     return token;
-  }   
+  }
+
+  public static validateToken = async (req: Req, res: Res, next: Next): Promise<void> => {
+    try {
+      const token = req.headers.authorization;
+      if (!token) throw TOKEN_NOTFOUND;
+
+      try {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET as string) as IDecodedToken;
+        req.user = decodedToken;
+        return next();
+      } catch (error) {
+        throw TOKEN_INVALID;
+      }
+    } catch (error) {
+      return next(error);
+    }
+  };
 }
